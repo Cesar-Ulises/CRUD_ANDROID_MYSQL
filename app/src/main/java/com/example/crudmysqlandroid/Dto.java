26a2fdf7c301b0c703cd356c2f491df6bcb5cba9
Cesar-Ulises <56010602+Cesar-Ulises@users.noreply.github.com>
2019-11-08 13:57:16 -0600

    Corrigiendo errores

--- app/src/main/java/com/example/crudmysqlandroid/Dto.java
+++ app/src/main/java/com/example/crudmysqlandroid/Dto.java
@@ -24,5 +24,23 @@ public class Dto implements Serializable {
         return codigo;
     }
 
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
 
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
