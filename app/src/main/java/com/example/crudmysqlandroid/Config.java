13ef4f21b4af24389aae6811e13a5af3ef42348b
Cesar-Ulises <56010602+Cesar-Ulises@users.noreply.github.com>
2019-11-08 13:39:42 -0600

    Reparando el listar productos

--- app/src/main/java/com/example/crudmysqlandroid/Config.java
+++ app/src/main/java/com/example/crudmysqlandroid/Config.java
@@ -2,9 +2,9 @@ package com.example.crudmysqlandroid;
 
 public class Config {
 
-    /*
+
     //URL PARA ACCESO REMOTA A BASE DE DATOS MYSQL DEL SERVIDOR DEL PROF. GÁMEZ EN LA NUBE.
-    public static final String urlGuardar = "http://mjgl.com.sv/mysqlcrud/guardar.php";                               //Guardar datos DB.
+    /*public static final String urlGuardar = "http://mjgl.com.sv/mysqlcrud/guardar.php";                               //Guardar datos DB.
     public static final String urlEliminar = "http://mjgl.com.sv/mysqlcrud/eliminar.php";                             //Eliminar datos DB.
     public static final String urlActualizar = "http://mjgl.com.sv/mysqlcrud/actualizar.php";                          //Actualizar datos DB.
 
@@ -13,8 +13,8 @@ public class Config {
 
     public static final String urlConsultaCodigo = "http://mjgl.com.sv/mysqlcrud/buscarArticulosCodigo.php";            //Busquedas por código.
     public static final String urlConsultaDescripcion = "http://mjgl.com.sv/mysqlcrud/buscarArticulosDescripcion.php";  //Busquedas por descripción.
-    public static final String urlConsultaAllArticulos = "http://mjgl.com.sv/mysqlcrud/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
-    */
+    public static final String urlConsultaAllArticulos = "http://mjgl.com.sv/mysqlcrud/buscarArticulos.php";   */         //Devuelve todos los registros de la tabla MySQL.
+
 
     //URL PARA ACCESO A BASE DE DATOS LOCAL MYSQL.
     //public static final String urlGuardar = "http://192.168.43.46/mysqlcrud/guardar.php";                               //Guardar datos DB.
@@ -28,7 +28,7 @@ public class Config {
 
     public static final String urlConsultaCodigo = "http://192.168.43.134/mysqlcrud/buscarArticulosCodigo.php";            //Busquedas por código.
     public static final String urlConsultaDescripcion = "http://192.168.43.134/mysqlcrud/buscarArticulosDescripcion.php";  //Busquedas por descripción.
-    public static final String urlConsultaAllArticulos = "http://192.168.43.134/mysqlcrud/buscarArticulos.php";            //Devuelve todos los registros de la tabla MySQL.
+    public static final String urlConsultaAllArticulos = "http://192.168.43.134/mysqlcrud/buscarArticulos.php";        //Devuelve todos los registros de la tabla MySQL.
 
 
 }
